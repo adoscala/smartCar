84d1e9374a076b162d3ca0f58e0d98c40e52cf97
Juan Andres Mezzera <juan.a.mezzera@gmail.com>
2018-10-16 10:16:44 -0300

    Agrego clase evento

--- Event.py
+++ Event.py
@@ -1,1 +1,7 @@
 class Event:
+
+    def __init__(self, tipo, descripcion, hora, conductor):
+        self.tipo = tipo
+        self.descripcion = descripcion
+        self.hora = hora
+        self.conductor = conductor
